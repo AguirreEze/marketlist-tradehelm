024a2eaece6a02061b75b78b750fb30f89a98ada
AguirreEze <aguirre.eze95@gmail.com>
2022-02-16 19:17:59 -0300

    removed component

--- components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.scss"
-
-export default function Button({ children, disabled }) {
-  return (
-    <button className={styles.button} disabled={disabled}>
-      {children}
-    </button>
-  )
-}
