9a5ca0e9acbdfea79d589d5ab75c82b57f0c9622
AguirreEze <aguirre.eze95@gmail.com>
2022-02-18 11:49:36 -0300

    fix list on own component

---
+++ components/Item/index.js
@@ -0,0 +1,24 @@
+import styles from "./styles.module.scss"
+
+export default function Item({ data, listState }) {
+  const [list, setList] = listState
+
+  const removeItem = (e) => {
+    setTimeout(() => {
+      const updatedList = list.filter((item) => item.name !== e)
+      window.localStorage.setItem(
+        "marketlist-tydrok",
+        JSON.stringify(updatedList)
+      )
+      setList(updatedList)
+    }, 1000)
+  }
+  return (
+    <li className={styles.item}>
+      <span>{data.name}</span>
+      <button className={styles.delete} onClick={() => removeItem(data.name)}>
+        delete
+      </button>
+    </li>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import { useEffect, useState } from "react"
+import Item from "../components/Item"
 import ItemForm from "../components/ItemForm"
 import Modal from "../components/Modal"
 import styles from "./styles.module.scss"
@@ -13,16 +14,6 @@ export default function Home() {
     if (data) setList(JSON.parse(data))
   }, [])
 
-  const removeItem = (e) => {
-    setTimeout(() => {
-      const updatedList = list.filter((item) => item.name !== e)
-      window.localStorage.setItem(
-        "marketlist-tydrok",
-        JSON.stringify(updatedList)
-      )
-      setList(updatedList)
-    }, 1000)
-  }
   return (
     <div className={styles.background}>
       <Head>
@@ -34,19 +25,9 @@ export default function Home() {
         <h1 className={styles.title}>Supermarket list</h1>
         <h2 className={styles.counter}>{list.length} item(s)</h2>
         <ul className={styles.list}>
-          {list.map((e) => {
-            return (
-              <li key={e.name} className={styles.item}>
-                <span>{e.name}</span>
-                <button
-                  className={styles.delete}
-                  onClick={() => removeItem(e.name)}
-                >
-                  delete
-                </button>
-              </li>
-            )
-          })}
+          {list.map((e, index) => (
+            <Item data={e} key={index} listState={[list, setList]} />
+          ))}
         </ul>
         <button onClick={() => setShowModal(true)} className={styles.button}>
           Add item
