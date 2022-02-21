7f1dd9ac18dddd846b2c46342ea619d68ac243da
AguirreEze <aguirre.eze95@gmail.com>
2022-02-21 15:35:58 -0300

    Feat darkmode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import { useState, useEffect } from "react"
+import styles from "./layout.module.scss"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [darkTheme, setDarkTheme] = useState(false)
+
+  const handleChecked = (e) => {
+    setDarkTheme(e.target.checked)
+  }
+
+  useEffect(() => {
+    if (window.localStorage.getItem("checklist-theme") === "dark")
+      setDarkTheme(true)
+  }, [])
+
+  useEffect(() => {
+    if (darkTheme) {
+      document.documentElement.setAttribute("data-theme", "dark")
+      window.localStorage.setItem("checklist-theme", "dark")
+    } else {
+      document.documentElement.removeAttribute("data-theme")
+      window.localStorage.setItem("checklist-theme", "light")
+    }
+  }, [darkTheme])
+  return (
+    <>
+      <input
+        type="checkbox"
+        checked={darkTheme}
+        onChange={handleChecked}
+        className={styles.button}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
